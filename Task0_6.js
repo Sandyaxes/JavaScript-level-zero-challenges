3913279986ea952d8968b5f7def59ffd42690306
Sandyaxes <45788837+Sandyaxes@users.noreply.github.com>
2021-05-20 17:16:27 -0700

    Update Task0_6.js

--- Task0_6.js
+++ Task0_6.js
@@ -1,14 +1,13 @@
 //JavaScript Level-Zero-Coding-Challenges Task 0.6
 
-function max(num1, num2, num3){
-
-	if(num1 > num2 && num1 > num3){
-		return num1;
-	}else if(num2 > num1 && num2 > num3){
-		return num2;
-	}else{
-		return num3;
-	}
+function max(...num){
+  var max = 0;
+  for(var i in num){
+    if(max < num[i]){
+      max = num[i];
+    }
+  }
+  return max;
 }
 
 console.log("Maximum number is " + max(33, 89, 60));
