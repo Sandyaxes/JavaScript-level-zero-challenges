e8f8d2b7302d45fbe9bc9199b67a508a560e4628
Sandyaxes <45788837+Sandyaxes@users.noreply.github.com>
2021-05-21 02:48:20 -0700

    Update Task0_5.js

--- Task0_5.js
+++ Task0_5.js
@@ -3,8 +3,8 @@
 function TriangleArea(num1, num2, num3){
 
 	var semiPerimeter = (num1 + num2 + num3) / 2;
-	var area = (int)sqrt(semiPerimeter * (semiPerimeter - num1) * (semiPerimeter - num2) * (semiPerimeter - num3));
+	var area = Math.floor(Math.sqrt(semiPerimeter * (semiPerimeter - num1) * (semiPerimeter - num2) * (semiPerimeter - num3)));
 	return area;
 }
 
-console.log("Triangle area is " + TriangleArea(4,5,6) + " sq.units");
+console.log("Triangle area is " + TriangleArea(3,4,5) + " sq.units");
