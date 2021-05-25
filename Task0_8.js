288d52e79c52ef166c1d5426a6201693f53db740
Sandyaxes <45788837+Sandyaxes@users.noreply.github.com>
2021-05-25 11:17:23 -0700

    Update Task0_8.js

--- Task0_8.js
+++ Task0_8.js
@@ -1,26 +1,20 @@
+function numToHours(inputNum) {
+  let hours = Math.floor(inputNum / 60);
+  let minutes = Math.floor(inputNum % 60);
 
-//JavaScript Level-Zero-Coding-Challenges Task 0.8
-
-function numToHours(inputNum){
- 
- var hours = Math.floor(inputNum /60);
-  var minutes = Math.floor(inputNum % 60);
-
-  if(hours === 1){
-    if(minutes === 1){
-      console.log(hours +" hour, " + minutes + " minute");
-    }else{
-      console.log(hours +" hour, " + minutes + " minutes");
-    }
-    
-  }else{
-    if(hours === 1){
-      console.log(hours +" hours, " + minutes + " minute");
-    }else{
-      console.log(hours +" hours, " + minutes + " minutes");
-    }
+  if (hours === 1 && minutes === 1) {
+    console.log(hours + " hour ," + minutes + " minute");
+  } else if (hours > 1 && minutes === 1) {
+    console.log(hours + " hours ," + minutes + " minute");
+  } else if (hours === 1 && minutes > 1) {
+    console.log(hours + " hour ," + minutes + " minutes");
+  } else if (hours === 0 && minutes === 1) {
+    console.log(hours + " hours ," + minutes + " minute");
+  } else if (hours === 0 && minutes === 1) {
+    console.log(hours + " hours ," + minutes + " minute");
+  } else {
+    console.log(hours + " hours ," + minutes + " minutes");
   }
-  
 }
 
-numToHours(133)
+numToHours(133);
