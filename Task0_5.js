9cf90f07d9cb260f37aee81e9c74d33b0651b4ee
Sandyaxes <45788837+Sandyaxes@users.noreply.github.com>
2021-05-25 09:38:18 -0700

    Update Task0_5.js

--- Task0_5.js
+++ Task0_5.js
@@ -1,10 +1,8 @@
-//JavaScript Level-Zero-Coding-Challenges Task 0.5
-
 function TriangleArea(num1, num2, num3){
 
 	var semiPerimeter = (num1 + num2 + num3) / 2;
 	var area = Math.floor(Math.sqrt(semiPerimeter * (semiPerimeter - num1) * (semiPerimeter - num2) * (semiPerimeter - num3)));
-	return area;
+	return  area ;
 }
 
-console.log("Triangle area is " + TriangleArea(3,4,5) + " sq.units");
+console.log(TriangleArea(3,4,5));
