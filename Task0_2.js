9d1e447a4b2c2a91cb5fecd7e72cf2fc940f8f93
Sandyaxes <45788837+Sandyaxes@users.noreply.github.com>
2021-05-27 18:02:48 -0700

    Update Task0_2.js

--- Task0_2.js
+++ Task0_2.js
@@ -5,4 +5,4 @@ let z = 1 + (1 + 2);
 let a = 1 + ((1 * 2) / 2);
 let b = (1 + 1 * 2) / 2;
 
-console.log("x = "+ x +"\ny = "+ y +"\nz = "+ z +"\na = "+ a +"\nb = "+ b);
+console.log(x +"\n"+y +"\n"+ z +"\n"+ a +"\n"+ b);
