c9dee961d261ebfea7f69844ff234d4f61ebf7e9
Sandyaxes <45788837+Sandyaxes@users.noreply.github.com>
2021-06-02 02:22:38 -0700

    Update Task0_8.js

--- Task0_8.js
+++ Task0_8.js
@@ -3,20 +3,15 @@ function numToHours(inputNum) {
   let minutes = Math.floor(inputNum % 60);
 
   if (hours === 1 && minutes === 1) {
-    console.log(hours + " hour ," + minutes + " minute");
-  } else if (hours > 1 && minutes === 1) {
-    console.log(hours + " hours ," + minutes + " minute");
-  } else if (hours === 1 && minutes > 1) {
-    console.log(hours + " hour ," + minutes + " minutes");
-  } else if (hours > 1 && minutes === 1) {
-    console.log(hours + " hours ," + minutes + " minute");
-  } else if (hours === 1 && minutes === 0 ) {
-    console.log(hours + " hour ," + minutes + " minutes");
-  } else if (hours === 0 && minutes === 1) {
-    console.log(hours + " hours ," + minutes + " minute");
+    console.log(hours + " hour, " + minutes + " minute");
+  } else if (hours === 1 && (minutes === 0 || minutes > 1)) {
+    console.log(hours + " hour, " + minutes + " minutes");
+  } else if ((hours === 0 || hours > 1) && minutes === 1) {
+    console.log(hours + " hours, " + minutes + " minute");
   } else {
-    console.log(hours + " hours ," + minutes + " minutes");
+    console.log(hours + " hours, " + minutes + " minutes");
   }
 }
 
-numToHours(60);
+numToHours(500);
+
